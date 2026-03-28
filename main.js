ba499ba0d6cbbf52c0741cc3992fd35e08f376b7
ismoilshoilyosov8-ai <ismoilshoilyosov8@gmail.com>
2026-03-28 16:16:39 +0500

    continium

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let mbDiv = document.querySelector(".header__mb-div");
 let buttonBox = document.querySelector(".header__mb-buttons-box");
 let openBurger = document.querySelector(".header__mb-open-burger-button");
 let closeBurger = document.querySelector(".header__mb-close-burger-button");
-let mbButton = document.querySelector(".header__mb-button");
-let links = document.querySelectorAll(".header__mb-texts");
+let accordionBoxes = document.querySelectorAll(".faq__mini-bottom-boxes");
+let accordionIcons = document.querySelectorAll(".faq__botttom-icons");
+let accordionTexts = document.querySelectorAll(".faq__texts");
+let accordionMiniBoxes = document.querySelectorAll(".faq__mini-bottom-boxes");
 buttonBox.addEventListener("click", (e) => {
   e.preventDefault();
   mbDiv.classList.toggle("active");
@@ -14,4 +16,15 @@ buttonBox.addEventListener("click", (e) => {
     openBurger.style.display = "block";
     closeBurger.style.display = "none";
   }
+});
+accordionBoxes.forEach((item, index) => {
+  let accordionText = accordionTexts[index];
+  let accordionIcon = accordionIcons[index];
+  let accordionMiniBox = accordionMiniBoxes[index];
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    accordionText.classList.toggle("active");
+    accordionMiniBox.classList.toggle("active");
+    accordionIcon.classList.toggle("active");
+  });
 });
